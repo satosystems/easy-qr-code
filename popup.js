7fe131d76d987891878475e37016fd7c1ea8dc14
Satoshi Ogata <satosystems@gmail.com>
2019-04-15 14:17:11 +0900

    add first version
    最初の実装を追加。

---
+++ popup.js
@@ -0,0 +1,19 @@
+(function() {
+  var textarea = document.getElementById("textarea"),
+      button = document.getElementById("button"),
+      image = document.getElementById("image"),
+      makeUrl = function(text) {
+        return "http://chart.googleapis.com/chart?cht=qr&chs=400x400&choe=UTF-8&chl=" + encodeURIComponent(text);
+      };
+  chrome.tabs.getSelected(null, function(tab) {
+    image.src = makeUrl(tab.url);
+    textarea.value = tab.url;
+    textarea.select();
+    button.onclick = function() {
+      var text = textarea.value;
+      if (text !== "") {
+        image.src = makeUrl(text);
+      }
+    };
+  });
+})();
